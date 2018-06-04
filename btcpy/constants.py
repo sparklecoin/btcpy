ff42cfcb38f278fef589eae10c717431f5c96f57
sparklecoin <dev@sparklecoin.com>
2018-06-04 20:37:10 +0000

    sparklecoin constants

--- btcpy/constants.py
+++ btcpy/constants.py
@@ -3,12 +3,13 @@ from decimal import Decimal
 
 class Constants(object):
 
-    _lookup = {'base58.prefixes': {'P': ('p2pkh', 'mainnet'),
-                                   'm': ('p2pkh', 'testnet'),
-                                   'p': ('p2sh', 'mainnet')},
-               'base58.raw_prefixes': {('mainnet', 'p2pkh'): bytearray(b'\x37'),
-                                       ('testnet', 'p2pkh'): bytearray(b'\x6f'),
-                                       ('mainnet', 'p2sh'): bytearray(b'\x75'),
+    _lookup = {'base58.prefixes': {'S': ('p2pkh', 'mainnet'),
+                                   't': ('p2pkh', 'testnet'),
+                                   's': ('p2sh', 'mainnet'),
+                                   '2': ('p2sh', 'testnet')},
+               'base58.raw_prefixes': {('mainnet', 'p2pkh'): bytearray(b'\x3f'),
+                                       ('testnet', 'p2pkh'): bytearray(b'\x7f'),
+                                       ('mainnet', 'p2sh'): bytearray(b'\x7d'),
                                        ('testnet', 'p2sh'): bytearray(b'\xc4')},
                'bech32.net_to_hrp': {'mainnet': 'bc',
                                      'testnet': 'tb'},
@@ -17,7 +18,7 @@ class Constants(object):
                'xkeys.prefixes': {'mainnet': 'x', 'testnet': 't'},
                'xpub.version': {'mainnet': b'\x04\x88\xb2\x1e', 'testnet': b'\x04\x35\x87\xcf'},
                'xprv.version': {'mainnet': b'\x04\x88\xad\xe4', 'testnet': b'\x04\x35\x83\x94'},
-               'wif.prefixes': {'mainnet': 0xb7, 'testnet': 0xef},
+               'wif.prefixes': {'mainnet': 0xbf, 'testnet': 0xff},
                'decimals': Decimal('1e6')
                }
 
